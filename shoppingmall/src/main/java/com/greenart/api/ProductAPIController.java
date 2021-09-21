5f1e8ab028dd00561b16dc29e5a36a5060d2c4d1
HarryBonoPark <hyery95@naver.com>
2021-09-22 00:43:38 +0900

    관리자 상품

--- shoppingmall/src/main/java/com/greenart/api/ProductAPIController.java
+++ shoppingmall/src/main/java/com/greenart/api/ProductAPIController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,6 +46,27 @@ public class ProductAPIController {
         service.deleteProductInfo(seq);
 
         resultMap.put("message", "상품이 삭제되었습니다.");
+        return resultMap;
+    }
+
+    @GetMapping("/product/get")
+    public Map<String, Object> getProductInfo(@RequestParam Integer seq) {
+        Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+
+        ProductVO vo = service.selectProductInfoBySeq(seq);
+        resultMap.put("status", true);
+        resultMap.put("data", vo);
+
+        return resultMap;
+    }
+    @PatchMapping("/product/update")
+    public Map<String, Object> updateProductInfo(@RequestBody ProductVO vo) {
+        Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+
+        service.updateProductInfo(vo);
+        resultMap.put("status", true);
+        resultMap.put("message", "수정되었습니다.");
+
         return resultMap;
     }
 }
--- shoppingmall/src/main/webapp/assets/js/product.js
+++ shoppingmall/src/main/webapp/assets/js/product.js
@@ -9,7 +9,7 @@ $(function() {
     });
     
     $("#close").click(function(){
-        if(confirm("입력을 취소하시겠습니까?\n(저장하지 않은 정보는 모두 사라집니다.)")){
+        if(confirm("입력을 취소하시겠습니까?\n저장하지 않은 정보는 모두 사라집니다.")){
             $(".product_form").css("display","");
             $(".product_form input").val("");
             $(".product_form select option:first-child").prop("selected", true);
@@ -88,6 +88,81 @@ $(function() {
         $.ajax({
             type:"post",
             url:"/product/regist",
+            data:JSON.stringify(data),
+            contentType:"application/json",
+            success:function(r){
+                alert(r.message);
+                location.reload();
+            }
+        }) 
+    })
+
+    $("#modify").click(function(){
+        
+        let pi_name = $("#product_name").val();
+        if(pi_name == "" || pi_name == null || pi_name == undefined) {
+            alert("상품명을 입력하세요");
+            return;
+        }
+        let pi_price = $("#product_price").val();
+        if(pi_price == "" || pi_price == null || pi_price == undefined) {
+            alert("가격을 입력하세요");
+            return;
+        }
+        let pi_cate_seq = $("#product_category option:selected").val();
+        if(pi_cate_seq == "" || pi_cate_seq == "" || pi_cate_seq == undefined) {
+            alert("카테고리를 선택하세요");
+            return;
+        }
+        let pi_stock = $("#product_stock").val();
+        if(pi_stock == "" || pi_stock == null || pi_stock == undefined) {
+            alert("재고를 입력하세요");
+            return;
+        }
+        let pi_si_seq = $("#product_seller option:selected").val();
+        if(pi_si_seq == "" || pi_si_seq == "" || pi_si_seq == undefined) {
+            alert("판매자를 선택하세요");
+            return;
+        }
+        let pi_discount_rate = $("#product_discount").val();
+        if(pi_discount_rate == "" || pi_discount_rate == null || pi_discount_rate == undefined) {
+            alert("할인률을 입력하세요");
+            return;
+        }
+        let pi_point_rate = $("#product_point").val();
+        if(pi_point_rate == "" || pi_point_rate == null || pi_point_rate == undefined) {
+            alert("적립률을 입력하세요");
+            return;
+        }
+        let pi_caution = $("#product_caution").val();
+        let pi_weight = $("#product_weight").val();
+        if(pi_weight == "" || pi_weight == null || pi_weight == undefined) {
+            alert("무게를 입력하세요");
+            return;
+        }
+        let pi_di_seq = $("#product_delivery option:selected").val();
+        if(pi_di_seq == "" || pi_di_seq == "" || pi_di_seq == undefined) {
+            alert("배송업체를 선택하세요");
+            return;
+        }
+
+        let data = {
+            pi_seq:$(this).attr("mod-seq"),
+            pi_name:pi_name,
+            pi_price:pi_price,
+            pi_cate_seq:pi_cate_seq,
+            pi_stock:pi_stock,
+            pi_si_seq:pi_si_seq,
+            pi_discount_rate:pi_discount_rate,
+            pi_point_rate:pi_point_rate,
+            pi_caution:pi_caution,
+            pi_weight:pi_weight,
+            pi_di_seq:pi_di_seq,
+            pi_img_uri:$("#img_preview").attr("img-uri")
+        }
+        $.ajax({
+            type:"patch",
+            url:"/product/update",
             data:JSON.stringify(data),
             contentType:"application/json",
             success:function(r){
@@ -182,6 +257,40 @@ $(function() {
                             }
                         })
                     }
+                })
+                $(".product_modify").click(function() {
+                    $("#save").css("display", "none");
+                    $("#modify").css("display", "block");
+                    $(".product_form > h1").html("상품 수정");
+                    $(".product_form").css("display", "block");
+
+                    let seq = $(this).attr("data-seq");
+                    $("#modify").attr("mod-seq", seq);
+                    $.ajax({
+                        type:"get",
+                        url:"/product/get?seq="+seq,
+                        success:function(r) {
+                            console.log(r);
+                            $("#product_name").val(r.data.pi_name);
+                            $("#product_price").val(r.data.pi_price);
+                            $("#product_category").val(r.data.pi_cate_seq);
+                            $("#product_seller").val(r.data.pi_si_seq);
+                            $("#product_stock").val(r.data.pi_stock);
+                            $("#product_weight").val(r.data.pi_weight);
+                            $("#product_discount").val(r.data.pi_discount_rate);
+                            $("#product_point").val(r.data.pi_point_rate);
+                            $("#product_delivery").val(r.data.pi_di_seq);
+                            $("#product_caution").val(r.data.pi_caution);
+
+                            $("#pi_img_uri").html("");
+                            if(r.data.pi_img_uri != null) {
+                                $("#img_preview").html(
+                                    '<img src="/image/'+r.data.pi_img_uri+'"img-uri="'+r.data.pi_img_uri+'">'
+                                )
+                                $("#img_preview").attr("img-uri", r.data.pi_img_uri);
+                            }
+                        }
+                    })
                 })
             }
         })
